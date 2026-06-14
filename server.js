1670e933af9c4db6e58c35009c218076ce1ead01
lhecker51 <lisamariehecker83@gmail.com>
2026-06-14 17:26:47 +0200

    test minimal server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const http = require("http");
 
+const PORT = process.env.PORT || 3000;
+console.log("PORT env variable is:", process.env.PORT);
+console.log("listening on:", PORT);
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>it works</h1>");
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("server running on port", process.env.PORT || 3000);
-});
+server.listen(PORT);
