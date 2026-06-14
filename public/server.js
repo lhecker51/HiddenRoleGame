0cff3f4c9e072608e44bc331a978f594c946e5cc
lhecker51 <lisamariehecker83@gmail.com>
2026-06-14 16:45:37 +0200

    node.js

---
+++ public/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("player connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("player left:", socket.id);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("server running");
+});
