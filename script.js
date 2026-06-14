c6d431610122ecb1c1fd427429af744825a53a12
ugjki <ugjki@student.kit.edu>
2026-06-14 15:48:48 +0200

    test

---
+++ script.js
@@ -0,0 +1,7 @@
+let numButtonClicks = 0;
+function buttonClicked() {
+    numButtonClicks = numButtonClicks + 1;
+    document.getElementById("mainDiv").textContent =
+        "Button Clicked times: " + numButtonClicks;
+}
+console.log("Hidden Role Game prototype loaded");
