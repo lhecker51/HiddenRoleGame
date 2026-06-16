bad6c1786bc2119dd7adb9596f4c1f18976c5edb
lhecker51 <lisamariehecker83@gmail.com>
2026-06-16 12:40:24 +0200

    Bug?

--- server.ts
+++ server.ts
@@ -20,11 +20,11 @@ const villagerRole: Role = new Role("Villager");
 const werewolfRole: Role = new Role("Werewolf");
 
 class Player {
-    socket: Socket;
+    socket: typeof Socket;
     name: string;
     role: Role = villagerRole;
 
-    constructor(socket: Socket, name: string) {
+    constructor(socket: typeof Socket, name: string) {
         this.socket = socket;
         this.name = name;
     }
@@ -39,7 +39,7 @@ const sessions: Record<string, Session> = {};
 
 app.use(express.static("public"));
 
-io.on("connection", (socket: Socket) => {
+io.on("connection", (socket: typeof Socket) => {
     console.log("Player connected:", socket.id);
 
     socket.on("join_session", ({player_name, session_code}) => {
